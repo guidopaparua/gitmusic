12cef0ff12dacc8f3f5f9b0607c4b5ac3627032d
Guido Agustín Papa Rúa <guidopaparua@hotmail.com>
2022-07-23 16:44:58 -0300

    ACA ESTA FUNCIONANDO, PERDON JE

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const path = require("path");
-
-
 const app = express();
 const publicPath = path.resolve(__dirname, "./public");
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(publicPath));
 
@@ -14,6 +9,7 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("Servidor iniciado en: http://localhost:" + port);
 });
+
 app.get("/", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
 });
