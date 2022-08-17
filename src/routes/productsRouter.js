751f73abf6f5e0fe1f6752c7ce6d343dc5a28500
JuanPabloGrasso <juampigra2702@gmail.com>
2022-08-16 22:24:30 -0300

    subo controladores

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 
+// ************ Controller Require ************
 const productsController = require('../controllers/productsController');
 
+// ************ Configuración de multer ************
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/images/products')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+const upload = multer({ storage });
+
+/*** Devolver todos los productos ***/
 router.get('/', productsController.index);
+
+/*** Crear un producto ***/ 
 router.get('/create', productsController.create);
 router.post('/create', productsController.store);
+
+/*** Devolver un producto ***/ 
 router.get('/:id', productsController.detail);
+
+/*** Editar un producto ***/
 router.get('/:id/edit', productsController.edit);
 router.put('/:id', productsController.update);
+
+/*** Eliminar un producto***/ 
 router.delete('/:id', productsController.destroy);
 
 
