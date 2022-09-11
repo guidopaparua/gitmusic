3a0c0b50ff7e62e496561f3c4bf6563696a01b6b
hamwee <santiagohamwee@gmail.com>
2022-09-11 11:23:45 -0300

    eliminé controllers demás, empecé a dinamizar index

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const controller = {
     index: (req, res) => {
-        res.render('index');
+        const instrumentos = products.filter(product => product.productType=='instrumento')
+        res.render('index', {instrumentos});
     }
 };
 
