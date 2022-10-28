55451c839ddbb45397ee55f0045ac3ace63a4c3c
Guido Agustín Papa Rúa <guidopaparua@hotmail.com>
2022-10-27 23:03:58 -0300

    acomodando carpeta database y creando la funcion search

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.index);
+router.get('/search', mainController.search);
 
 module.exports = router;
