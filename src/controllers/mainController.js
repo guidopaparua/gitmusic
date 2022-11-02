24e4e88d6d1fad53633c25284412a62878d6a495
Guido Agustín Papa Rúa <guidopaparua@hotmail.com>
2022-11-01 21:52:19 -0300

    cambios con lucas

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const {Op} = require('sequelize');
+const db = require('../database/models');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const productsCategoryFilePath = path.join(__dirname, '../data/productsDataBaseCategory.json');
@@ -14,11 +16,11 @@ const controller = {
     search: (req, res) => {
         db.Products.findAll({
             where: {
-                title: { [Op.like]: `%${req.query.search}%` }
+                name: { [Op.like]: `%${req.query.search}%` }
             }
         })
-            .then(function (productos) {
-                res.render('products', { products })
+            .then(function (instrumentos) {
+                res.render('instrumentos', { instrumentos })
             })
             .catch(error => {
                 console.log(error)
