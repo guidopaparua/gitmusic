b891ca9b23cb0c39c3cd212533521c31622df3d7
lSeijasFerraudo <lucasgabrielseijas@gmail.com>
2022-11-06 11:40:36 -0300

    traer product desde db

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,7 +5,11 @@ const db = require('../database/models');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const productsCategoryFilePath = path.join(__dirname, '../data/productsDataBaseCategory.json');
+
+
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
 const productsCategory = JSON.parse(fs.readFileSync(productsCategoryFilePath, 'utf-8'));
 
 const controller = {
@@ -27,9 +31,26 @@ const controller = {
                 res.send(500);
             })
     },
-    instrumentos: (req, res) => {
-        const instrumentos = products
-        res.render('instrumentos', { instrumentos });
+    // aca hay un ejemplo para traer desde la db
+    // instrumentos: async (req, res) => {
+    //     res.render('instrumentos', { 
+    //         instrumentos: await  db.Product.findAll()
+    //         .then(instrumento => {
+    //             return instrumento;
+    //         })
+    //      });
+    // }
+
+    // aca hay un ejemplo para traer desde la db
+    instrumentos: (req, res) =>{
+        db.Product.findAll()
+        .then( (products) =>{
+            return res.render('instrumentos', {products})
+        })
+        .catch(error =>{
+            console.log(error)
+            res.send(500);
+        })
     }
 };
 
--- src/routes/registerRouter.js
+++ src/routes/registerRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const userController = require('../controllers/userController');
 const { body } = require('express-validator');
 const validaciones = [
@@ -9,7 +11,23 @@ const validaciones = [
     body('password').notEmpty().withMessage('Este campo es obligatorio')
 ];  
 
+
+// ************ Configuración de multer ************
+const storage = multer.diskStorage({
+    destination:(req, file, cb)=>{
+        
+        cb(null, 'public/images/users')
+    },
+    filename:(req, file, cb)=>{
+        let imageName = Date.now() + path.extname(file.originalname);
+        cb(null, imageName)
+    }
+})
+
+const upload = multer({ storage });
+
+
 router.get('/', userController.register)
-router.post('/', validaciones, userController.processRegister);
+router.post('/', upload.single('imagen'), userController.processRegister);
 
 module.exports = router;
