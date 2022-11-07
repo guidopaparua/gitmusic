c008d7fc2f57a7c670d9b7c5bc0c0562fe997bc4
JuanPabloGrasso <juampigra2702@gmail.com>
2022-11-07 00:11:36 -0300

    subo vista de ofertas

--- src/controllers/ofertasController.js
+++ src/controllers/ofertasController.js
@@ -1,13 +1,22 @@
 const fs = require('fs');
 const path = require('path');
+const db = require('../database/models/');
 
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const controller = {
     ofertas: (req, res) => {
-        const instrumentos = products;
-        res.render('ofertas', { instrumentos });
+        db.Product.findAll({
+            where: {
+                discount: 15 //not Null
+            }
+        })
+        .then( (products) =>{
+            return res.render('ofertas', {products})
+        })
+        .catch(error =>{
+            console.log(error)
+            res.send(500);
+        })
     }
 };
 
