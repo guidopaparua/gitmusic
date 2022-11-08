d1fb78471c95600122dd2558fa7aa7736e2463cb
Guido Agustín Papa Rúa <guidopaparua@hotmail.com>
2022-11-07 23:33:12 -0300

    proyecto funcionando, en el anterior estaba roto

--- src/app.js
+++ src/app.js
@@ -20,9 +20,6 @@ const productsRouter = require('./routes/productsRouter');
 const registerRouter = require('./routes/registerRouter');
 const ofertasRouter = require('./routes/ofertasRouter');
 const instrumentosRouter = require('./routes/instrumentosRouter');
-app.use((req, res, next) => {
-    res.status(404).render('notFound')
-  })
 //DATABASE
 const DbProductsRouter = require('./routes/DbProductsRouter');
 const DbUsersRouter = require('./routes/DbUsersRouter');
